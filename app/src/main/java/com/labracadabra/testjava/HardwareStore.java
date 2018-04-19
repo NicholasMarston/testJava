c2a487cb424f172de72df61112020047ae38a9d0
Nicholas Marston <nhm6692@uncw.edu>
2018-04-18 23:03:01 -0400

    This includes the working hardware store button

---
+++ app/src/main/java/com/labracadabra/testjava/HardwareStore.java
@@ -0,0 +1,13 @@
+package com.labracadabra.testjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HardwareStore extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hardware_store);
+    }
+}
--- app/src/main/java/com/labracadabra/testjava/locationsSelect.java
+++ app/src/main/java/com/labracadabra/testjava/locationsSelect.java
@@ -1,14 +1,18 @@
 package com.labracadabra.testjava;
 
 import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class locationsSelect extends AppCompatActivity {
 
     private Button backButton;
+
+    private ImageButton hardware;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +28,31 @@ public class locationsSelect extends AppCompatActivity {
                 launchActivity();
             }
         });
+        //
+        hardware = (ImageButton) findViewById(R.id.hardwareButton);
+
+        hardware.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                launchActivity2();
+            }
+        });
     }
+
+
+
+
+
 
     private void launchActivity() {
 
         Intent intent = new Intent(this, profiles.class);
+        startActivity(intent);
+    }
+    private void launchActivity2() {
+
+        Intent intent = new Intent(this, HardwareStore.class);
         startActivity(intent);
     }
 }
